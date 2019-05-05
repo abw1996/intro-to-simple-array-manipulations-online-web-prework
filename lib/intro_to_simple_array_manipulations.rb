cec56ef2a965bb19cea174e4cbaa9bc5867bf96f
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-05-05 22:28:08 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,21 @@
+def using_push(place, thang)
+  place.push(thang)
+end
+def using_unshift(place, thang)
+  place.unshift(thang)
+end
+def using_pop(place)
+  place.pop
+end
+def pop_with_args(place)
+  place.pop(2)
+end
+def using_shift(place)
+  place.shift 
+end
+def shift_with_args(place)
+  place.shift(2)
+end
+def using_concat(place, thang)
+  place.concat(thang)
+end
